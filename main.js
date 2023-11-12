ac4e8ef73b08c7b15c0f3728659316ae4b853769
duffblackraven <t9164700668@gmail.com>
2023-11-12 20:00:44 +0100

    API_2.16

--- main.js
+++ main.js
@@ -1,150 +1,39 @@
-import { getFetchApi, postFetchApi } from "./Api.js";
-import { getCorrectDate } from "./GetDate.js";
-import { renderComms } from "./RenderComms.js";
+import { getFetchApi } from "./api.js";
+import { renderLogin } from "./loginPage.js";
+import { renderComms } from "./renderComments.js";
 
-const commEl = document.getElementById("addComm");
-const buttonEl = document.getElementById("addButton");
-const nameEl = document.getElementById("addName");
-const textEl = document.getElementById("addText");
-const loaderEl = document.getElementById("pageLoader");
-
-
-let commentsArr = [];
-
-const getApi = () => {
-  commEl.classList.add("hidden");
-  loaderEl.classList.remove("hidden");
-
-getFetchApi()
-.then((responseData) => {
-    console.log (responseData)
-    const arrayComments = responseData.comments.map((comment) => {
-    return {
-      name: comment.author.name,
-      date: getCorrectDate(comment.date),
-      comment: comment.text,
-      like: comment.likes,
-      isLiked: false
-    };
-  });
-  commentsArr = arrayComments;
-  renderComms({commEl, commentsArr, replyToComment, initEventListener, deleteCommentButton});
-  //console.log(arrayComments);
-})
-.then(() => {
-commEl.classList.remove("hidden");
-loaderEl.classList.add("hidden");
-})
-.catch((error)=> {
-    if (error.message === 'Failed to fetch') {
-    alert ('Ошибка интернет соединения, попробуйте позже')
-    } else {
-    alert (error.message);
-    }
-  })
-};
-getApi();
-
-const replyToComment = () => {
-
-    const newComms = document.querySelectorAll('.comment');
     
-    for (const newComm of newComms) {
-    
-      newComm.addEventListener ('click', () => {
-      textEl.value = `${newComm.dataset.text} ${newComm.dataset.username}`
-      });
-     }
-    };
-    const initEventListener = () => {
-        const likesButton = document.querySelectorAll('.like-button');
-      
-        for (const likeButtonEl of likesButton) {
-      
-          likeButtonEl.addEventListener ('click', (event) => {
-            event.stopPropagation();
-          const index = likeButtonEl.dataset.index;
-      
-            if (commentsArr[index].isLiked === false) {
-              commentsArr[index].isLiked = true;
-              commentsArr[index].like++;
-            } else {
-              commentsArr[index].isLiked = false;
-              commentsArr[index].like--;
-            }
-            //console.log(commentsArr[index].isLiked)
-            renderComms({commEl, commentsArr, replyToComment, initEventListener, deleteCommentButton});
-          });
-        }
-      };
-      const deleteCommentButton = () => {
-        const deleteComments = document.querySelectorAll('.delete-comment');
-      
-        for (const deleteComment of deleteComments) {
-          deleteComment.addEventListener("click", (event) => {
-            event.stopPropagation();
-      
-            const index = deleteComment.dataset.index;
-            commentsArr.splice(index, 1);
-            renderComms({commEl, commentsArr, replyToComment, initEventListener, deleteCommentButton});
-          });
-        }
-      };
-renderComms({commEl, commentsArr, replyToComment, initEventListener, deleteCommentButton});
+    const newDate = new Date;
+    export const formatedDate = `${newDate.getDate()}.${newDate.getMonth() + 1}.${newDate.getFullYear()} ${newDate.getHours()}:${newDate.getMinutes()}`;
 
-buttonEl.addEventListener("click", () => {
-
-nameEl.classList.remove('error');
-
-  if (nameEl.value === "") {
-  nameEl.classList.add('error');
-  return;
-};
-
-textEl.classList.remove('error');
-
-  if (textEl.value === "") {
-  textEl.classList.add('error');
- return;
-}
-
-buttonEl.disabled = true;
-buttonEl.textContent = "Добавляем..."
-
-postFetchApi(nameEl, textEl)
-.then((response) => {
-console.log(response.status);
-if (!response.ok) {
-  if (response.status === 400) {
-  throw new Error('Ошибка ввода');
-} 
-if (response.status === 500) {
-  throw new Error('Ошибка на сервере');
-}
-else {
-  //alert ('Доступ к тайнам человечества закрыт, попробуйте позже');
-  throw new Error ('Доступ к тайнам человечества закрыт, попробуйте позже');
+    const date = ({ apiDate }) => {
+      return new Date(apiDate).toLocaleDateString() + " "
+          + (new Date(apiDate).getHours() < 10 ? '0' + new Date(apiDate).getHours() : new Date(apiDate).getHours()) + ":"
+          + (new Date(apiDate).getMinutes() < 10 ? '0' + new Date(apiDate).getMinutes() : new Date(apiDate).getMinutes()) + ":"
+          + (new Date(apiDate).getSeconds() < 10 ? '0' + new Date(apiDate).getSeconds() : new Date(apiDate).getSeconds())
   }
-}
-})
-.then(() => {
-getApi();
-nameEl.value = '';
-textEl.value = '';
-})
-.catch((error)=> {
-if (error.message === 'Failed to fetch') {
-  alert ('Ошибка интернет соединения, попробуйте позже')
-} 
-if (error.message === 'Ошибка ввода') {
-  alert ('Ошибка колличества символов в имени/комментарии... Пожалуйста, введите не менее 3х символов')
-} 
-if (error.message === 'Ошибка на сервере') {
-  alert ('Ошибка на сервере, возвращайтесь позже')
-}
-})
-.finally(() => {
-buttonEl.disabled = false;
-buttonEl.textContent = "Написать";
-})
-});
+
+    export let comments = [];
+    export const getRenderComments = () => {
+    getFetchApi()
+    .then((responseData) => {
+        comments = responseData.comments.map((comment) => {
+          const apiDate = comment.date;
+          return {
+            name: comment.author.name,
+            date: date({ apiDate }),
+            text: comment.text,
+            likes: comment.likes,
+            isLiked: false,
+          }
+        });
+    
+      renderComms({ comments });
+    // loaderComment.style.display = 'none';
+
+      });
+      
+    };
+
+    getRenderComments();
+    renderLogin({ getRenderComments });
